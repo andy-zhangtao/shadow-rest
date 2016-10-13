e5d759a4700881db3ecba0d24da72edea2b28e24
andy <andy.zhangtao@hotmail.com>
2016-10-13 16:04:13 +0800

    当关闭网络链接时,如果此时还存在未断开的链接，会存在伪链接的情况

--- shadowsocks/handler/versionHandler.go
+++ shadowsocks/handler/versionHandler.go
@@ -9,7 +9,7 @@ import (
 // GetVersion 获取当前版本信息
 func GetVersion(w http.ResponseWriter, r *http.Request) {
 
-	dv := "Dev Version: " + "r16 2b30bd9"
+	dv := "Dev Version: " + "r18 a248c7b"
 	rv := "  Release Version: 0.1"
 	Sandstorm.HTTPSuccess(w, dv+rv)
 }
