c9c07e38fe0855df6fb4a88b701b43bf42b431dc
andy <andy.zhangtao@hotmail.com>
2016-10-18 11:19:50 +0800

    重新提交

--- shadowsocks/handler/versionHandler.go
+++ shadowsocks/handler/versionHandler.go
@@ -9,7 +9,7 @@ import (
 // GetVersion 获取当前版本信息
 func GetVersion(w http.ResponseWriter, r *http.Request) {
 
-	dv := "Dev Version: " + "r26M 99c2cd7"
+	dv := "Dev Version: " + "r28+2 3831110"
 	rv := "  Release Version: 0.1"
 	Sandstorm.HTTPSuccess(w, dv+rv)
 }
